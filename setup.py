e88dd7da1c6a265efbd789a189508339a6b8f0a4
Rafael de la Fuente <rafael.fuente.herrezuelo@gmail.com>
2021-03-15 03:05:43 +0100

    update

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ How the method and the simulator work is described in this [Article](https://raf
 
 setup(
     name='diffractsim',
-    version='1.1.1',
+    version='1.2.0',
     description='Implementation of the Angular Spectrum method in Python to simulate Diffraction Patterns',
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -19,22 +19,16 @@ setup(
     keywords = ['diffraction', 'angular spectrum method', 'optics', 'physics simulation'],
     author='Rafael de la Fuente',
     author_email='rafael.fuente.herrezuelo@gmail.com',
-    license='MIT',
+    license='BSD 3-Clause',
     packages=['diffractsim'],
     install_requires=['numpy', 'scipy', 'Pillow', 'matplotlib', 'progressbar'],
     classifiers=[
-        # How mature is this project? Common values are
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
         'Development Status :: 5 - Production/Stable',
 
-        # Indicate who your project is intended for
         'Intended Audience :: Science/Research',
         'Topic :: Scientific/Engineering',
 
-        # Pick your license as you wish (should match "license" above)
-        'License :: OSI Approved :: MIT License',
+        'License :: OSI Approved :: BSD 3-Clause License',
         'Operating System :: OS Independent',
 
     ],
