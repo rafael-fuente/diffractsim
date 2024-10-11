67ecf61fdd10dfa2780b0ffec911ab15cab633ba
LtqxWYEG <44180911+LtqxWYEG@users.noreply.github.com>
2024-10-11 18:15:18 +0200

    Some changes

--- examples/run_ffmpeg_manually.py
+++ examples/run_ffmpeg_manually.py
@@ -1,23 +1,16 @@
 import ffmpeg
 
-framerate = 20
+framerate = 25
 start_frame = 1
 path = "./animation/"
 filename = "frame_"
 filetype = ".png"  # Can save as jpg or png
 
-# frame2_000070.png
-
+# pip install ffmpeg-python // Add ffmpeg.exe to system path variable (Or just put in same folder)
 (
-    ffmpeg  # pip install ffmpeg-python // Put ffmpeg.exe in same folder (or maybe in a path variable)
-    .input(path + filename + '%06d' + filetype, start_number = start_frame, framerate = framerate)  # Note: pattern_type='glob' and "*" wildcard only works in linux
-    .output(path + 'animation.mp4')
+    ffmpeg
+    .input(path + filename + '%06d' + filetype, start_number = start_frame, framerate = framerate)  # Note: pattern_type='glob' and "*" wildcard only work in linux
+    .output(path + 'animation.mp4', vcodec='png', movflags='faststart')  # crf=1, **{'qscale:v': 1}
     .run(overwrite_output = True)
+    # for Android use: vcodec='libvpx-vp9', pix_fmt='yuv420p', video_bitrate=200000000, movflags='faststart'
 )
-
-# (
-#     ffmpeg  # pip install ffmpeg-python // Put ffmpeg.exe in same folder (or maybe in a path variable)
-#     .input('./animation/frame_%04d.png', framerate=24)  # Note: pattern_type='glob' and "*" wildcard only works in linux
-#     .output('./animation/animation.mp4')
-#     .run(overwrite_output=True)
-# )
