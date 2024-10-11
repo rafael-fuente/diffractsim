4fdfff5553aebc2635023afb333a94aa763911e1
LtqxWYEG <44180911+LtqxWYEG@users.noreply.github.com>
2024-10-11 18:48:59 +0200

    Add files via upload

--- examples/create_animation.py
+++ examples/create_animation.py
@@ -77,8 +77,9 @@ if __name__ == "__main__":
 
     if encode_to_video:
         (
-            ffmpeg  # pip install ffmpeg-python // Put ffmpeg.exe in same folder (or maybe in a path variable)
-            .input(path + filename + '%06d' + filetype, start_number = start_frame, framerate=framerate)  # Note: pattern_type='glob' and "*" wildcard only works in linux
-            .output(path + 'animation.mp4')
-            .run(overwrite_output=True)
+            ffmpeg
+            .input(path + filename + '%06d' + filetype, start_number = start_frame, framerate = framerate)  # Note: pattern_type='glob' and "*" wildcard only work in linux
+            .output(path + 'animation.mp4', vcodec = 'png', movflags = 'faststart')  # crf=1, **{'qscale:v': 1}
+            .run(overwrite_output = True)
+            # for Android use: vcodec='libvpx-vp9', pix_fmt='yuv420p', video_bitrate=200000000, movflags='faststart'
         )
