485509d54dfb0f71667c54057d69c3fc3769b23b
Rafael de la Fuente <rafael.fuente.herrezuelo@gmail.com>
2025-09-15 03:01:14 +0200

    update

--- examples/usaf_30cm_phase_hologram_generation_and_reconstruction.py
+++ examples/usaf_30cm_phase_hologram_generation_and_reconstruction.py
@@ -1,7 +1,8 @@
 import diffractsim
 diffractsim.set_backend("JAX")
 
-from diffractsim import MonochromaticField, mm, nm, cm, CustomPhaseRetrieval, ApertureFromImage
+from diffractsim import MonochromaticField, mm, nm, cm
+from diffractsim import SLM, load_image_as_function, load_file_as_function, load_phase_as_function
 
 
 #Note: CustomPhaseRetrieval requires autograd which is not installed by default with diffractsim. 
@@ -12,33 +13,29 @@ from diffractsim import MonochromaticField, mm, nm, cm, CustomPhaseRetrieval, Ap
 distance = 30*cm
 PR = CustomPhaseRetrieval(wavelength=532 * nm, z = distance, extent_x=30 * mm, extent_y=30 * mm, Nx=2048, Ny=2048)
 
-PR.set_source_amplitude(amplitude_mask_path= "./apertures/white_background.png", image_size=(15.0 * mm, 15.0 * mm))
-PR.set_target_amplitude(amplitude_mask_path= "./apertures/USAF_test.png", image_size=(15.0 * mm, 15.0 * mm))
+PR.set_source_amplitude(load_image_as_function("./apertures/white_background.png", 
+                                               x_interval = (-15*mm/2, +15*mm/2),   y_interval = (-15*mm/2, +15*mm/2)))
+PR.set_target_amplitude(load_image_as_function("./apertures/USAF_test.png", 
+                                               x_interval = (-15*mm/2, +15*mm/2),   y_interval = (-15*mm/2, +15*mm/2)))
 
-PR.retrieve_phase_mask(max_iter = 15, method = 'Adam-Optimizer')
+PR.retrieve_phase_mask(max_iter = 1000, method = 'Adam-JAX')
 PR.save_retrieved_phase_as_image('USAF_hologram.png')
+PR.save_retrieved_phase_as_file('USAF_hologram.npy')
 
 
-
-#Add a plane wave
 
 F = MonochromaticField(
     wavelength=532 * nm, extent_x=30 * mm, extent_y=30 * mm, Nx=2048, Ny=2048, intensity = 0.001
 )
 
 
-F.add(ApertureFromImage(
-     amplitude_mask_path= "./apertures/white_background.png", 
-     image_size=(15.0    * mm, 15.0  * mm), simulation = F)
+# add Spatial Light Modulator
+F.add(SLM(
+      phase_mask_function = load_phase_as_function("USAF_hologram.npy", 
+      x_interval = (-15*mm, +15*mm),   y_interval = (-15*mm, +15*mm)), 
+      size_x =15*mm, size_y =15*mm, 
+      simulation = F)
 )
-
-
-
-F.add(ApertureFromImage(
-     phase_mask_path= "USAF_hologram.png", 
-     image_size=(30.0   * mm, 30.0 * mm), simulation = F)
-)
-
 
 
 # plot phase at z = 0
