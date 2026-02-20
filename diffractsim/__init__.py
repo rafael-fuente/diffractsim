d8439409820c508a6ad3e17cbdb513e62f205dec
Solari Systems <solarisys2025@gmail.com>
2026-02-20 16:33:19 -0500

    feat: Vectorial EM field model
    Closes #69

--- diffractsim/__init__.py
+++ diffractsim/__init__.py
@@ -1,10 +1,10 @@
 from .util.backend_functions import get_backend, set_backend
 from .util.backend_functions import backend as bd
-from .util.backend_functions import backend as bd
 from .util.image_handling import load_image_as_function
 from .util.file_handling import load_file_as_function, load_phase_as_function
 from .polychromatic_simulator import PolychromaticField
 from .monochromatic_simulator import MonochromaticField
+from .monochromatic_field import VectorialField, LinearPolarizer, HalfWavePlate, QuarterWavePlate
 from . import colour_functions as cf
 from .polynomials import zernike_polynomial
 from .holography import FourierPhaseRetrieval, CustomPhaseRetrieval, RotationalPhaseDesign
@@ -12,3 +12,23 @@ from .diffractive_elements import *
 from .light_sources import *
 
 from .util.constants import *
+
+__all__ = [
+    'get_backend',
+    'set_backend',
+    'bd',
+    'load_image_as_function',
+    'load_file_as_function',
+    'load_phase_as_function',
+    'PolychromaticField',
+    'MonochromaticField',
+    'VectorialField',
+    'LinearPolarizer',
+    'HalfWavePlate',
+    'QuarterWavePlate',
+    'cf',
+    'zernike_polynomial',
+    'FourierPhaseRetrieval',
+    'CustomPhaseRetrieval',
+    'RotationalPhaseDesign',
+]
