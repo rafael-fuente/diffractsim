4df9db28e0aa3459fc26031bae3325851cbddd7c
zahouanimalek1-eng <zahouanimalek1@gmail.com>
2026-02-23 10:21:39 +0100

    Update double_slit_interference_farfield.py

--- examples/double_slit_interference_farfield.py
+++ examples/double_slit_interference_farfield.py
@@ -1,18 +1,15 @@
 import diffractsim
-diffractsim.set_backend("CPU") #Change the string to "CUDA" to use GPU acceleration
+diffractsim.set_backend("CPU")
 
 from diffractsim import MonochromaticField, nm, mm, cm, RectangularSlit
 
-F = MonochromaticField(
-    wavelength = 632.8 * nm, extent_x=20. * mm, extent_y=20. * mm, Nx=2048, Ny=2048, intensity =2.
-)
+# Initialize field with polarization support (pol='x')
+F = MonochromaticField(wavelength=632.8*nm, extent_x=15.*mm, extent_y=15.*mm, Nx=1024, Ny=1024, pol='x')
 
+D = 1 * mm  # slits separation
+F.add(RectangularSlit(width=0.2*mm, height=1.5*mm, x0=-D/2, y0=0) + RectangularSlit(width=0.2*mm, height=1.5*mm, x0=D/2, y0=0))
 
-D = 1 * mm  #slits separation
-F.add(RectangularSlit(width = 0.2*mm, height = 1.5*mm, x0 = -D/2 , y0 = 0)   +   RectangularSlit(width = 0.2*mm, height = 1.5*mm, x0 = D/2, y0 = 0))
-
-α, β, radiant_intensity_percos = F.get_farfield()
-
-F.plot_farfield(α, β, radiant_intensity_percos ,  grid = True,  alpha_lim=[-0.01,0.01], beta_lim=[-0.01,0.01])
-
-F.plot_farfield_spherical_coordinates(α, β, radiant_intensity_percos ,  theta_lim=0.5)
+# Compute and plot far-field diffraction
+alpha, beta, radiant_intensity_percos = F.get_farfield()
+F.plot_farfield(alpha, beta, radiant_intensity_percos, grid=True, alpha_lim=[-0.01,0.01], beta_lim=[-0.01,0.01])
+F.plot_farfield_spherical_coordinates(alpha, beta, radiant_intensity_percos, theta_lim=0.5)
